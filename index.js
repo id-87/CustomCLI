d3f9a8d585f704d2c05c1844a1950dfb47421c14
[Harshit Gupta] <hrstgpt87@gmail.com>
2026-02-26 11:28:47 +0530

    Shebang operator

--- index.js
+++
@@ -1,8 +0,0 @@
-var Command = require('commander').Command;
-var program = new Command();
-program
-    .command("greet <name>")
-    .action(function (name) {
-    console.log("Hello ".concat(name));
-});
-program.parse();
